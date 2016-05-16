828bbb093a66852e2971b6969a3526c3326a4432
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-16 01:34:53 -0400

    Pretty colors

---
+++ js/init.js
@@ -0,0 +1,22 @@
+$(function(){
+ var mult = 2; // Ratio of page size to window size
+ var range_of_tilt = 90; // Angle through which the board can rotate
+ var $filler = $('#filler');
+ $filler.width(mult*100+'%');
+ $filler.height(mult*100+'%');
+
+ var $doc = $(document);
+ var $window = $(window);
+ $doc.scrollTop($window.height() * (mult-1) / 2);
+ $doc.scrollLeft($window.width() * (mult-1) / 2);
+
+ $(window).scroll(function(){
+   // x and y range from -0.5 to 0.5
+   var x = $window.scrollLeft() / ($window.width() * (mult-1)) - 0.5;
+   var y = $window.scrollTop() / ($window.height() * (mult-1)) - 0.5;
+   $('#board').css({
+     transform: 'translateZ(-100px) rotateX('+y*range_of_tilt+'deg) rotateY('+x*range_of_tilt+'deg)',
+     background: 'hsl(' +(100*(x+0.5)+250)+ ', 33%, ' +(50*(y+0.5)+50)+ '%)'
+   });
+ });
+});
