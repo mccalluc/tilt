ab1e817af0ab136bf754c847369cb5609fc84021
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-16 16:39:41 -0400

    chain

--- js/init.js
+++ js/init.js
@@ -2,13 +2,13 @@ $(function(){
  var mult = 2; // Ratio of page size to window size
  var range_of_tilt = 90; // Angle through which the board can rotate
  var $filler = $('#filler');
- $filler.width(mult*100+'%');
- $filler.height(mult*100+'%');
+ $filler.width(mult*100+'%')
+        .height(mult*100+'%');
 
  var $doc = $(document);
  var $window = $(window);
- $doc.scrollTop($window.height() * (mult-1) / 2);
- $doc.scrollLeft($window.width() * (mult-1) / 2);
+ $doc.scrollTop($window.height() * (mult-1) / 2)
+     .scrollLeft($window.width() * (mult-1) / 2);
 
  $(window).scroll(function(){
    // x and y range from -0.5 to 0.5
