59438feed54e7e0860d6352fb78d7a79e227ff4d
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-16 17:07:36 -0400

    plausible shading on ball.

--- js/init.js
+++ js/init.js
@@ -1,3 +1,7 @@
+function color(x, y, s, l) {
+  return 'hsl(' +(100*(x+0.5)+250)+ ', ' +s+ '%, ' +(50*(y+0.5)+l)+ '%)';
+}
+
 $(function(){
  var mult = 2; // Ratio of page size to window size
  var range_of_tilt = 90; // Angle through which the board can rotate
@@ -16,11 +20,12 @@ $(function(){
    var y = $window.scrollTop() / ($window.height() * (mult-1)) - 0.5;
    $('#board').css({
      transform: 'translateZ(-200px) rotateX('+y*range_of_tilt+'deg) rotateY('+x*range_of_tilt+'deg)',
-     background: 'hsl(' +(100*(x+0.5)+250)+ ', 33%, ' +(50*(y+0.5)+35)+ '%)'
+     background: color(x, y, 33, 35)
    });
    $('.ball').css({
-     transform: 'translateZ(50px) rotateX('+(360-y*range_of_tilt)+'deg) rotateY('+(360-x*range_of_tilt)+'deg)'
+     transform: 'translateZ(50px) rotateX('+(360-y*range_of_tilt)+'deg) rotateY('+(360-x*range_of_tilt)+'deg)',
      // z: ball radius; x and y reverse rotation so flat to viewer.
+     background: 'radial-gradient(at 20% top,'+color(x,y,10,50)+','+color(x,y,10,20)+')'
    });
  });
 });
