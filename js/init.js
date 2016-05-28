bdfc45213b16570b707d7bcf6d3e9a77e3b77135
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-28 17:44:09 -0400

    whitespace / intro message.

--- js/init.js
+++ js/init.js
@@ -1,15 +1,16 @@
-$(function(){
- var mult = 2; // Ratio of page size to window size
- var $filler = $('#filler');
- $filler.width(mult*100+'%')
-        .height(mult*100+'%');
-        
+$(function () {
+    var mult = 2; // Ratio of page size to window size
+    var $filler = $('#filler');
+    $filler.width(mult * 100 + '%')
+            .height(mult * 100 + '%');
 
 
- var $doc = $(document);
- var $window = $(window);
- $doc.scrollTop($window.height() * (mult-1) / 2)
-     .scrollLeft($window.width() * (mult-1) / 2);
 
+    var $doc = $(document);
+    var $window = $(window);
+    $doc.scrollTop($window.height() * (mult - 1) / 2)
+            .scrollLeft($window.width() * (mult - 1) / 2);
 
+    $('<p id="intro">Scroll to tilt.<br>Best on modern browsers.<br>Sorry.</p>')
+            .appendTo('#board').fadeOut(20000);
 });
